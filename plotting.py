83495af237ed2e437290c4f7fc5ecf7ed6e858bc
Cosmas Nyairo <nyairocosmas@gmail.com>
2020-04-06 12:08:46 +0300

    Hotfix + Added hover effect on generated graphs

--- plotting.py
+++ plotting.py
@@ -1,7 +1,12 @@
 from motion_detector import datetime, os, df, current_time
 from bokeh.plotting import figure, output_file, show
-
+from bokeh.models import HoverTool,ColumnDataSource
 print('Generating graph')
+
+df["Start_Motion"]=df["START"].dt.strftime("%Y-%m-%d %H:%M:%S")
+df["End_Motion"]=df["END"].dt.strftime("%Y-%m-%d %H:%M:%S")
+
+cds=ColumnDataSource(df)
 
 p = figure(x_axis_type='datetime', plot_height=100,
            plot_width=500, title='Motion Graph')
@@ -17,9 +22,12 @@ p.xaxis.minor_tick_line_color = None
 p.yaxis.minor_tick_line_color = None
 
 
-p.xaxis.axis_label="Duration"
+p.xaxis.axis_label = "Duration"
 
-q = p.quad(left=df["START"], right=df["END"], bottom=0, top=1, color="green")
+q = p.quad(left="START", right="END", bottom=0, top=1, color="blue",source=cds)
+
+hover = HoverTool(tooltips=[("START", "@Start_Motion"), ("END", "@End_Motion")])
+p.add_tools(hover)
 
 filename = current_time.strftime('%Y-%m-%d-%H-%M-%S')
 path1 = 'html_files'
